5893c59ced0462a267c1c1d70f93a45f8266e102
Nadir0glu <nadiroglunail@gmail.com>
2024-02-08 11:35:19 -0500

    Final Solution Nail

--- src/index.js
+++ src/index.js
@@ -1,29 +1,99 @@
 // index.js
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
+const url = "http://localhost:3000/ramens"
+fetch(url)
+.then((res) => res.json())
+.then((data) => {
+  data.forEach(imagesData => {
+      displayRamens(imagesData)
+      console.log(imagesData);
+  });;
+})
+
+
+const displayRamens = (imagesData) => {
+  // request data from the server display image for each for an image add #ramen-menu
+  let displayDiv = document.getElementById("ramen-menu")
+  let image = document.createElement("img")
+  image.src = imagesData.image
+  displayDiv.append(image)
+  
+
+
+  image.addEventListener('click', function handleClick() {
+    console.log("Click");
+    let ramenName = document.querySelector(".name")
+    ramenName.textContent = imagesData.name
+
+    let restaurant = document.querySelector(".restaurant")
+    restaurant.textContent = imagesData.restaurant
+
+
+    let imageD = document.querySelector(".detail-image")
+    imageD.src = imagesData.image
+
+    let rating = document.querySelector("#rating-display")
+    rating.textContent = imagesData.rating
+
+    let comment = document.querySelector("#comment-display")
+    comment.textContent = imagesData.comment
+
+  })
+
+  
 };
+  let form = document.querySelector("#new-ramen")
+  let inputName = document.querySelector("#new-name")
+  let inputImg = document.querySelector("#new-image")
+  let inputRestaurant = document.querySelector("#new-restaurant")
+  let inputRating = document.querySelector("#new-rating")
+  let inputComment = document.querySelector("#new-comment")
 
-const addSubmitListener = () => {
-  // Add code
-}
+form.addEventListener("submit", function addNew(e) {
+  e.preventDefault()
+  console.log("NewClick");
 
-const displayRamens = () => {
-  // Add code
-};
+  let newRamen = {
+    name: inputName.value,
+    image: inputImg.value,
+    restaurant: inputRestaurant.value,
+    rating: inputRating.value,
+    comment: inputComment.value
+  }
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+  displayRamens(newRamen)
+  // let newRamenName = document.createElement("h2")
+  // newRamenName.textContent = inputName.value
 
-main()
+  // let newRamenImg = document.createElement("img")
+  // newRamenImg.src = inputImg.value
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+  // let newRamenRestaurant = document.createElement("h3")
+  // newRamenRestaurant.textContent = inputRestaurant.value
+
+  // let newRamenRating = document.createElement("p")
+  // newRamenRating.textContent = inputRating.value
+
+  // let newRamenComment = document.createElement("p")
+  // newRamenComment.textContent = inputComment.value
+
+  // displayDiv.appendChild(newRamenImg);
+  // displayDiv.appendChild(newRamenName);
+  // displayDiv.appendChild(newRamenRestaurant);
+  // displayDiv.appendChild(newRamenRating);
+  // displayDiv.appendChild(newRamenComment);
+})
+// const main = () => {
+//   // Invoke displayRamens here
+//   // Invoke addSubmitListener here
+// }
+
+// main()
+
+// // Export functions for testing
+// export {
+//   displayRamens,
+//   addSubmitListener,
+//   // handleClick,
+//   main,
+// };
